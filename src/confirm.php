94ee5725e22d693c59aed7b0043a03f46ed357d7
(sengoku) <(a15dc095@dhw.ac.jp)>
2017-05-11 17:55:15 +0900

    confirm.php footerを削除

--- src/confirm.php
+++ src/confirm.php
@@ -88,16 +88,6 @@ $_SESSION = $_POST;
             </main>
           </div>
         </div>
-
-        <footer class="MasterFoot">
-          <div class="Container">
-            <ul class="sp-sns-menu">
-              <li class="sp-sns-menu__list"><a href="#" class="sp-sns-menu__link sp-sns-menu__link--white"><i class="fa fa-twitter" aria-hidden="true"></i></a></li>
-              <li class="sp-sns-menu__list"><a href="#" class="sp-sns-menu__link sp-sns-menu__link--white"><i class="fa fa-facebook" aria-hidden="true"></i></a></li>
-            </ul>
-            <p class="MasterFoot__Copyright"><small>Copyright&copy;2017 H&amp;C FILMS All Rights Reserved</small></p>
-          </div>
-        </footer>
       </div>
     </div>
 
