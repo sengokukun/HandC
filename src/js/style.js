338bf7d53cc1facf40bf00dfc23f37201e0d297b
(sengoku) <(a15dc095@dhw.ac.jp)>
2017-05-22 19:45:01 +0900

    アバウトアニメーションを変更

--- src/js/style.js
+++ src/js/style.js
@@ -97,16 +97,42 @@ $(function() {
   });
 
   if (windowW > 840) {
-    $('.MasterHead__List--works').hover(function() {
-      $('.PcWorkskinds').fadeIn("fast");
-    }, function() {
-      $('.PcWorkskinds').fadeOut("fast");
-    });
-    $('.ModalPoint').on('inview', function() {
-      $('.Modal').on('inview', function(event, isInView) {
-        $(this).stop().addClass('ModalSlide');
-      });
-    });
+    var Modal__about = function(){
+      var tl = new TimelineMax(),
+        tl2 = new TimelineMax(),
+        tl3 = new TimelineMax(),
+        tl4 = new TimelineMax(),
+        tl5 = new TimelineMax(),
+        bdL = $(this).find('.bdL'),
+        bdB = $(this).find('.bdB'),
+        bdR = $(this).find('.bdR'),
+        bdT = $(this).find('.bdT'),
+        FadeIn = $(this).find('.FadeIn')
+
+      tl.to(bdL, .3, {
+        height:"100%"
+      }),
+      tl2.to(bdB, .3, {
+        width: "100%",
+        delay: .2
+      }),
+      tl3.to(bdR, .3, {
+        height: "100%",
+        delay: .4
+      }),
+      tl4.to(bdT, .3, {
+        width: "100%",
+        delay: .6
+      })
+      tl5.to(FadeIn, .3, {
+        opacity: 1,
+        delay: 1
+      })
+    };
+
+    $('.FadeIn__1').parent('.Modal').on('inview', Modal__about);
+    $('.FadeIn__2').parent('.Modal').on('inview', Modal__about);
+    $('.FadeIn__3').parent('.Modal').on('inview', Modal__about);
   }
 
   $(window).on("load", function() {
