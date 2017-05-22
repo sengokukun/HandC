904a59fc24ddc76cdd90c074f6def9e72ff971f2
(sengoku) <(a15dc095@dhw.ac.jp)>
2017-05-23 07:50:15 +0900

    アバウト レスポンシブ対応

--- src/input.php
+++ src/input.php
@@ -154,7 +154,7 @@
       </div>
       <footer class="MasterFoot">
         <div class="Container">
-          <ul class="sp-sns-menu">
+          <ul class="sp-sns-menu jcC">
             <li class="sp-sns-menu__list"><a href="#" class="sp-sns-menu__link sp-sns-menu__link--white"><i class="fa fa-twitter" aria-hidden="true"></i></a></li>
             <li class="sp-sns-menu__list"><a href="#" class="sp-sns-menu__link sp-sns-menu__link--white"><i class="fa fa-facebook" aria-hidden="true"></i></a></li>
           </ul>
