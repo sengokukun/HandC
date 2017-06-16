9bf8e53eb116dc5ac1330b74fb741b27a8f20602
(sengoku) <(a15dc095@dhw.ac.jp)>
2017-06-16 18:20:02 +0900

    wp-loading画面追加

---
+++ System/wp-content/handc/loading.php
@@ -0,0 +1,6 @@
+<!-- ローディング画面 -->
+<div class="Loading">
+  <div class="Loading__Gif">
+    <img src="/img/hc_loading_short.gif" alt="Loading..." width="300" height="300">
+  </div>
+</div><!-- ローディング画面ここまで -->
