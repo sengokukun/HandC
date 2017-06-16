dc18dbd3b5c508e72df244230a88b5c458089784
(sengoku) <(a15dc095@dhw.ac.jp)>
2017-06-16 17:50:09 +0900

    wp-footerを追加。 index.phpにfooter（top用）を追加

--- System/wp-content/handc/index.php
+++ System/wp-content/handc/index.php
@@ -1,1 +1,14 @@
 <?php get_header(); ?>
+
+
+
+<!-- フッター -->
+<footer class="MasterFoot">
+  <div class="Container">
+    <ul class="sp-sns-menu jcC">
+      <li class="sp-sns-menu__list"><a href="https://twitter.com/HandCFilms" class="sp-sns-menu__link sp-sns-menu__link--white" target="_blank"><i class="fa fa-twitter" aria-hidden="true"></i></a></li>
+      <li class="sp-sns-menu__list"><a href="https://www.facebook.com/hcfilms.official/" class="sp-sns-menu__link sp-sns-menu__link--white" target="_blank"><i class="fa fa-facebook" aria-hidden="true"></i></a></li>
+    </ul>
+    <p class="MasterFoot__Copyright"><small>Copyright&copy;2017 H&amp;C FILMS All Rights Reserved</small></p>
+  </div>
+</footer><!-- フッターここまで -->
