a7f245a7d4d5cae72721ed99f807440968041a48
(sengoku) <(a15dc095@dhw.ac.jp)>
2017-06-16 18:10:10 +0900

    wp-global-navを追加

--- System/wp-content/handc/index.php
+++ System/wp-content/handc/index.php
@@ -1,14 +1,18 @@
 <?php get_header(); ?>
 
-
-
-<!-- フッター -->
-<footer class="MasterFoot">
-  <div class="Container">
-    <ul class="sp-sns-menu jcC">
-      <li class="sp-sns-menu__list"><a href="https://twitter.com/HandCFilms" class="sp-sns-menu__link sp-sns-menu__link--white" target="_blank"><i class="fa fa-twitter" aria-hidden="true"></i></a></li>
-      <li class="sp-sns-menu__list"><a href="https://www.facebook.com/hcfilms.official/" class="sp-sns-menu__link sp-sns-menu__link--white" target="_blank"><i class="fa fa-facebook" aria-hidden="true"></i></a></li>
-    </ul>
-    <p class="MasterFoot__Copyright"><small>Copyright&copy;2017 H&amp;C FILMS All Rights Reserved</small></p>
+<div class="Gloval">
+  <div class="Top">
+    <?php get_template_part('global-nav'); ?>
+    <!-- フッター -->
+    <footer class="MasterFoot">
+      <div class="Container">
+        <ul class="sp-sns-menu jcC">
+          <li class="sp-sns-menu__list"><a href="https://twitter.com/HandCFilms" class="sp-sns-menu__link sp-sns-menu__link--white" target="_blank"><i class="fa fa-twitter" aria-hidden="true"></i></a></li>
+          <li class="sp-sns-menu__list"><a href="https://www.facebook.com/hcfilms.official/" class="sp-sns-menu__link sp-sns-menu__link--white" target="_blank"><i class="fa fa-facebook" aria-hidden="true"></i></a></li>
+        </ul>
+        <p class="MasterFoot__Copyright"><small>Copyright&copy;2017 H&amp;C FILMS All Rights Reserved</small></p>
+      </div>
+    </footer><!-- フッターここまで -->
   </div>
-</footer><!-- フッターここまで -->
+  <script src="/js/style.js"></script>
+</div>
